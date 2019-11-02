9246c592730d176b7ff7337ee16294e891e00ae4
Yuri Dimitre Dias de Faria <yuri.d.d.faria@gmail.com>
2019-11-01 22:29:59 -0300

    criado componente para o listbuilder do horario

---
+++ lib/app/screens/choiceHorario/components/listBuilder.dart
@@ -0,0 +1,25 @@
+import 'package:aula_online/app/screens/choiceHorario/choiceHorario.dart';
+import 'package:aula_online/app/shared/models/Professor.dart';
+import 'package:flutter/material.dart';
+
+final List<Professor> professores = [
+  Professor(
+      nome: "Yuri",
+      avatarURL: "https://randomuser.me/api/portraits/men/83.jpg",
+      bio: "Cansado",
+      horarios: ["9:30", "8:20"],
+      materia: "Programacao")
+];
+Widget listBuilder() {
+  return ListView.builder(
+    itemBuilder: listTile,
+    itemCount: professores[0].horarios.length,
+  );
+}
+
+Widget listTile(BuildContext context, int index) {
+  bool _isChecked;
+  return ListTile(
+    title: Text(professores[0].horarios[index]),
+  );
+}
