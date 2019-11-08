0e790aa467b565529601fbb15ab9f2f5a8f83b71
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-07 21:23:38 -0300

    adicionado campo de preco de aula

--- lib/app/shared/models/Professor.dart
+++ lib/app/shared/models/Professor.dart
@@ -3,6 +3,13 @@ class Professor {
   String materia;
   String avatarURL;
   String bio;
+  double precoAula;
   List<String> horarios;
-  Professor({this.nome, this.materia, this.avatarURL, this.bio, this.horarios});
+  Professor(
+      {this.nome,
+      this.materia,
+      this.avatarURL,
+      this.bio,
+      this.horarios,
+      this.precoAula});
 }
