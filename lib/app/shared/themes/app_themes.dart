75787b1f4dc6aa9acd88988ff273a11c8f95e917
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-21 00:41:53 -0300

    add themes

---
+++ lib/app/shared/themes/app_themes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final appThemeData = {
+  "OrangeLight": ThemeData(
+    primarySwatch: Colors.orange,
+  ),
+  "OrangeDark": ThemeData(
+    primaryColor: Colors.orange[700],
+    brightness: Brightness.dark,
+  )
+};
