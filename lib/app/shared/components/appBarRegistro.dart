70cbbfc38f0d8ea10c7f9614cbb7a4d34b3b00a5
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-21 00:47:01 -0300

    create button to change the theme

--- lib/app/shared/components/appBarRegistro.dart
+++ lib/app/shared/components/appBarRegistro.dart
@@ -1,11 +1,22 @@
+import 'package:aula_online/app/shared/blocs/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Widget title() {
   return AppBar(
     title: Text("Me salva ae"),
     centerTitle: true,
-    actions: <Widget>[
-      Icon(Icons.ac_unit),
-    ],
+    actions: <Widget>[IconButtonChangeTheme()],
   );
+}
+
+class IconButtonChangeTheme extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final tema = Provider.of<ThemeChanger>(context);
+    return IconButton(
+      icon: Icon(Icons.dashboard),
+      onPressed: () => tema.switchTheme(),
+    );
+  }
 }
