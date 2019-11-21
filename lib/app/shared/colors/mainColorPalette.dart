5906f8c0d302936b38200b767927de5737865c5d
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-21 00:42:27 -0300

    deleting maincolorpalette

--- lib/app/shared/colors/mainColorPalette.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// class ColorsPalette {
-//   Color mainColor = Colors.orange;
-//   static Color mainColor get (int number){
-//       return this.mainColor[number];
-//   }
-// }
