4c9f9da8eb0f7729d5018e0e2010456c28af5581
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-21 00:46:01 -0300

    modify materialapp to support provider /change name of named routes to use enum / theme using prov

--- lib/app/mainWidget.dart
+++ lib/app/mainWidget.dart
@@ -8,34 +8,59 @@ import 'package:aula_online/app/screens/aulaAvulsa/resumoAvulsa/resumoAvulsa.dar
 import 'package:aula_online/app/screens/choiceScreen/choiceScreen.dart';
 import 'package:aula_online/app/screens/pacoteAulas/dashboard/dashboard.dart';
 import 'package:aula_online/app/screens/pacoteAulas/loginPage/loginPage.dart';
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
+import 'package:aula_online/app/shared/blocs/theme.dart';
+import 'package:aula_online/app/shared/models/Routes.dart';
+import 'package:aula_online/app/shared/themes/app_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:aula_online/app/screens/splashScreen/splashscreen.dart';
+import 'package:provider/provider.dart';
 
 class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeChanger>(
+          builder: (_) => ThemeChanger(appThemeData["OrangeLight"]),
+        ),
+        ChangeNotifierProvider<CompraAvulsaBloc>.value(
+          value: null,
+        )
+      ],
+      child: StartApp(),
+    );
+  }
+}
+
+class StartApp extends StatelessWidget {
+  const StartApp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tema = Provider.of<ThemeChanger>(context);
     return MaterialApp(
-      initialRoute: '/splashScreen',
+      initialRoute: Routes.SplashScreen.toString(),
       routes: {
-        '/splashScreen': (context) => SplashScreen(),
-        '/choiceScreen': (context) => ChoiceScreen(),
-        '/choiceMateria': (context) => ChoiceMateria(),
-        '/choiceProfessor': (context) => ChoiceProfessor(),
-        '/choiceHorario': (context) => ChoiceHorario(),
-        '/cadastroAvulsa': (context) => CadastroAvulsa(),
-        '/metodosPagamento': (context) => MetodosPagamento(),
-        '/cadastroCartao': (context) => CadastroCartao(),
-        '/resumoAvulsa': (context) => ResumoAvulsa(),
-        '/loginPage': (context) => LoginPage(),
-        '/dashboard': (context) => DashBoard(),
+        Routes.SplashScreen.toString(): (context) => SplashScreen(),
+        Routes.ChoiceScreen.toString(): (context) => ChoiceScreen(),
+        Routes.ChoiceMateria.toString(): (context) => ChoiceMateria(),
+        Routes.ChoiceProfessor.toString(): (context) => ChoiceProfessor(),
+        Routes.ChoiceHorario.toString(): (context) => ChoiceHorario(),
+        Routes.CadastroAvulsa.toString(): (context) => CadastroAvulsa(),
+        Routes.MetodoPagamento.toString(): (context) => MetodosPagamento(),
+        Routes.CadastroCartao.toString(): (context) => CadastroCartao(),
+        Routes.ResumoAvulsa.toString(): (context) => ResumoAvulsa(),
+        Routes.LoginPage.toString(): (context) => LoginPage(),
+        Routes.DashBoard.toString(): (context) => DashBoard(),
         // '/efetuarLogin': (context) => EfetuarLogin(),
       },
       debugShowCheckedModeBanner: false,
       title: "Aula online",
       //home: CadastroCartao(),
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
+      theme: tema.getTheme(),
     );
   }
 }
