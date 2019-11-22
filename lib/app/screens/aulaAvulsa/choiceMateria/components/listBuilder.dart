1238480537111d665e34abe1e78711f9a6060174
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:34:41 -0300

    implementacao de state  manager

--- lib/app/screens/aulaAvulsa/choiceMateria/components/listBuilder.dart
+++ lib/app/screens/aulaAvulsa/choiceMateria/components/listBuilder.dart
@@ -1,7 +1,9 @@
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
 import 'package:aula_online/app/shared/models/Routes.dart';
 import 'package:flutter/material.dart';
 import 'package:aula_online/app/shared/models/Materia.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:provider/provider.dart';
 
 final List<Materia> materias = [
   Materia(nome: "Fisica", icone: Icons.panorama_fish_eye),
@@ -10,12 +12,14 @@ final List<Materia> materias = [
 ];
 
 Widget listTileMateria(context, index) {
+  final avulsablock = Provider.of<CompraAvulsaBloc>(context);
   return ListTile(
-    title: Text(materias[index].nome),
-    leading: Icon(materias[index].icone),
-    onTap: () =>
-        Navigator.of(context).pushNamed(Routes.ChoiceProfessor.toString()),
-  );
+      title: Text(materias[index].nome),
+      leading: Icon(materias[index].icone),
+      onTap: () {
+        avulsablock.materia = materias[index].nome;
+        Navigator.of(context).pushNamed(Routes.ChoiceProfessor.toString());
+      });
 }
 
 Widget listBuilderMateria() {
