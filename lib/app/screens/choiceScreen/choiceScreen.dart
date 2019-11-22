d20b170c1124abf44ebe01240545332552f372ae
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:30:57 -0300

    corrigido merge

--- lib/app/screens/choiceScreen/choiceScreen.dart
+++ lib/app/screens/choiceScreen/choiceScreen.dart
@@ -1,11 +1,14 @@
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
 import 'package:aula_online/app/shared/components/containerDashboard.dart';
 import 'package:aula_online/app/shared/models/Routes.dart';
 import 'package:flutter/material.dart';
 import 'package:aula_online/app/screens/choiceScreen/components/buttons.dart';
+import 'package:provider/provider.dart';
 
 class ChoiceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final aa = Provider.of<CompraAvulsaBloc>(context);
     return Scaffold(
       body: containerDashboard(
         context,
@@ -18,34 +21,10 @@ class ChoiceScreen extends StatelessWidget {
                 child: Image.asset("assets/lifesaver-hi.png"),
               ),
             ),
-<<<<<<< HEAD
-            flatButtonChoiceScreen(context,
+            choiceScreenButton(context,
                 text: "Aula Avulsa", route: Routes.ChoiceMateria.toString()),
-            Stack(
-              alignment: Alignment.centerRight,
-              children: <Widget>[
-                flatButtonChoiceScreen(context,
-                    text: "Pacote de Aulas",
-                    route: Routes.LoginPage.toString()),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: Text(
-                    "10% de Desconto",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                    ),
-                  ),
-                  decoration: BoxDecoration(color: Colors.red),
-                ),
-              ],
-            ),
-=======
             choiceScreenButton(context,
-                text: "Aula Avulsa", route: "/choiceMateria"),
-            choiceScreenButton(context,
-                text: "Pacote de Aulas", route: "/loginPage"),
->>>>>>> 7453853ae8d62ca7d2b396b3f497a61bf7cc21c4
+                text: "Pacote de Aulas", route: Routes.LoginPage.toString()),
           ],
         ),
       ),
