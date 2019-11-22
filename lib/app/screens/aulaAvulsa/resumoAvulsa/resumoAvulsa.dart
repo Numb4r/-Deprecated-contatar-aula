6f4ee68dd52b9f25478d7da3ff235803391eb8ee
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:31:48 -0300

    criado pagina de resumo da compra de aula avulsa

--- lib/app/screens/aulaAvulsa/resumoAvulsa/resumoAvulsa.dart
+++ lib/app/screens/aulaAvulsa/resumoAvulsa/resumoAvulsa.dart
@@ -1,17 +1,60 @@
+import 'package:aula_online/app/screens/aulaAvulsa/resumoAvulsa/components/TextResumo.dart';
+import 'package:aula_online/app/screens/choiceScreen/choiceScreen.dart';
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
+import 'package:aula_online/app/shared/components/SubmitButton.dart';
 import 'package:aula_online/app/shared/components/appBarRegistro.dart';
+import 'package:aula_online/app/shared/models/Professor.dart';
 import 'package:aula_online/app/shared/models/Routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ResumoAvulsa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final avulsa = Provider.of<CompraAvulsaBloc>(context, listen: false);
+    final Professor professor = avulsa.professor;
     return Scaffold(
-      appBar: title(),
-      body: FlatButton(
-        onPressed: () => Navigator.popUntil(
-            context, ModalRoute.withName(Routes.ChoiceHorario.toString())),
-        child: null,
-      ),
-    );
+        appBar: title(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 35),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "Resumo",
+                  style: TextStyle(fontSize: 45),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              TextResumo(type: "Professor", info: professor.nome),
+              TextResumo(type: "Materia", info: avulsa.materia),
+              TextResumo(type: "Horario", info: avulsa.horario),
+              TextResumo(type: "Nome", info: avulsa.nome),
+              TextResumo(type: "Email", info: avulsa.email),
+              TextResumo(type: "Telefone", info: avulsa.telefone),
+              TextResumo(
+                  type: "Tipo de pagamento",
+                  info: avulsa.creditCardPayment
+                      ? "Credit Card"
+                      : "Cash in hand"),
+              TextResumo(type: "Total", info: "R\$ ${professor.precoAula}"),
+              SubmitButton(
+                label: "Enviar",
+                onPressed: () => Navigator.popUntil(context,
+                    ModalRoute.withName(Routes.ChoiceScreen.toString())),
+              ),
+            ],
+          ),
+        ));
   }
 }
+
+// var _materia;
+// var _professor;
+// var _horario;
+// var _nome;
+// var _email;
+// var _telefone;
