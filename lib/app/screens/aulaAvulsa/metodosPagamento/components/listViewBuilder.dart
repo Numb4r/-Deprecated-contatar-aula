a1654bdbab0dd03266d85110c20c2e87877f4133
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:32:11 -0300

    arrumado rota do metodo de pagamento

--- lib/app/screens/aulaAvulsa/metodosPagamento/components/listViewBuilder.dart
+++ lib/app/screens/aulaAvulsa/metodosPagamento/components/listViewBuilder.dart
@@ -1,5 +1,7 @@
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
 import 'package:aula_online/app/shared/models/Routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 List<Map> metodos = [
   {"title": "Cash in hand", "icon": Icons.wb_iridescent},
@@ -7,12 +9,15 @@ List<Map> metodos = [
 ];
 
 Widget listTilePagamento(context, index) {
+  final avulsa = Provider.of<CompraAvulsaBloc>(context);
   return ListTile(
     title: Text(metodos[index]["title"]),
     leading: Icon(metodos[index]["icon"]),
     onTap: () {
-      Navigator.of(context)
-          .pushNamed(index == 0 ? '' : Routes.CadastroCartao.toString());
+      avulsa.creditCardPayment = index == 0 ? false : null;
+      Navigator.of(context).pushNamed(index == 0
+          ? Routes.ResumoAvulsa.toString()
+          : Routes.CadastroCartao.toString());
     },
   );
 }
