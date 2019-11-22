8f138f12ab9f61254fe02aac01d3ff2de37081b0
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:35:03 -0300

    implementacao de state manager

--- lib/app/screens/aulaAvulsa/choiceProfessor/choiceProfessor.dart
+++ lib/app/screens/aulaAvulsa/choiceProfessor/choiceProfessor.dart
@@ -1,13 +1,17 @@
 import 'package:aula_online/app/screens/aulaAvulsa/choiceProfessor/components/listBuilder.dart';
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
+
 import 'package:flutter/material.dart';
 import 'package:aula_online/app/shared/components/appBarRegistro.dart';
+import 'package:provider/provider.dart';
 
 class ChoiceProfessor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final avulsa = Provider.of<CompraAvulsaBloc>(context);
     return Scaffold(
       appBar: title(),
-      body: listBuilderProfessor(),
+      body: listBuilderProfessor(avulsa),
     );
   }
 }
