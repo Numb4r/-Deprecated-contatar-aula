8aee0f18fff0bb5961acc6b132687a3883e257d8
Numb4r <yuri.d.d.faria@gmail.com>
2019-11-22 00:34:14 -0300

    implementacao de state manager

--- lib/app/screens/aulaAvulsa/choiceHorario/choiceHorario.dart
+++ lib/app/screens/aulaAvulsa/choiceHorario/choiceHorario.dart
@@ -1,13 +1,17 @@
 import 'package:aula_online/app/screens/aulaAvulsa/choiceHorario/components/listBuilder.dart';
+import 'package:aula_online/app/shared/blocs/Compra_Avulsa_Bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:aula_online/app/shared/components/appBarRegistro.dart';
+import 'package:provider/provider.dart';
 
 class ChoiceHorario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final avulsa = Provider.of<CompraAvulsaBloc>(context);
+
     return Scaffold(
       appBar: title(),
-      body: listBuilder(),
+      body: listBuilder(avulsa),
     );
   }
 }
